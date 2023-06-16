025d84f62fee538cdb15cf37104f1678582ba862
adrian-cordero <adrian.corderomendez@outlook.com>
2023-06-16 17:03:31 -0600

    Adding file for Bus Interface and Display Interface

---
+++ busInterface.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+#
+# Title: Bus Movement Interface
+# Author: Adrian Cordero
+#
+# Description: 
---
+++ displayInterface.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+#
+# Title: Display Interface for Bus Data
+# Author: Adrian Cordero
+#
+# Description: 
